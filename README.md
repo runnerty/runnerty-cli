527bdcc886ca13ccf57083598ea2b5702ce88b39
Carlos Carmona Alcantara <carlos@iMac.local>
2020-05-03 17:35:12 +0200

    feature: upgrade to runnerty 2.6.0

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Runnerty CLI Tool
 
+<p align="center">
+  <a href="http://runnerty.io">
+    <img height="257" src="https://runnerty.io/assets/header/logo-stroked.png">
+  </a>
+  <p align="center">A new way for processes managing</p>
+</p>
+
 ## What is?
 
 Early version of Runnerty CLI
--- index.js
+++ index.js
@@ -1,55 +1,50 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
+const program = require("commander");
+const newProject = require("./src/new.js");
 
-const program = require('commander');
-const newProject = require('./src/new.js');
+// CHECK ARGS APP:
+program.version(
+  "Runnerty CLI " + require("./package.json").version,
+  "-v, --version"
+);
 
-// ASYNC INIT:
-(async () => {
-  // CHECK ARGS APP:
-  await program.version(
-    'Runnerty CLI ' + require('./package.json').version,
-    '-v, --version'
-  );
+// new:
+program
+  .command("new [project]")
+  .alias("n")
+  .description("create the project")
+  .action((project) => {
+    newProject(project);
+  })
+  .on("--help", () => {
+    console.log("");
+    console.log("Examples:");
+    console.log("");
+    console.log("  $rn new");
+    console.log("  $rn new my_runnerty_project");
+  });
 
-  // new:
-  await program
-    .command('new [project]')
-    .alias('n')
-    .description('create the project')
-    .action(project => {
-      newProject(project);
-    })
-    .on('--help', () => {
-      console.log('');
-      console.log('Examples:');
-      console.log('');
-      console.log('  $rn new');
-      console.log('  $rn new my_runnerty_project');
-    });
+program.parse(process.argv);
 
-  await program.parse(process.argv);
-})();
+// ==================================================================
 
-//==================================================================
-//
-
-program.on('command:*', () => {
+program.on("command:*", () => {
   console.error(
-    'Invalid command: %s\nSee --help for a list of available commands.',
-    program.args.join(' ')
+    "Invalid command: %s\nSee --help for a list of available commands.",
+    program.args.join(" ")
   );
   process.exit(1);
 });
 
-process.on('uncaughtException', err => {
-  console.error('error', err.stack);
+process.on("uncaughtException", (err) => {
+  console.error("error", err.stack);
 });
 
-process.on('unhandledRejection', (reason, p) => {
-  console.error('error', p, reason);
+process.on("unhandledRejection", (reason, p) => {
+  console.error("error", p, reason);
   process.exit();
 });
 
-process.on('SIGINT', () => {});
+process.on("SIGINT", () => {});
