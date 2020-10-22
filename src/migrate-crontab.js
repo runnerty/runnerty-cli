515e6268ad66e472a5a6aac3c40f051838bc796e
carlos <carloscarmonaalcantara@gmail.com>
2020-10-22 19:28:23 +0200

    build: 2.8.0

--- src/migrate-crontab.js
+++ src/migrate-crontab.js
@@ -9,13 +9,13 @@ const path = require('path');
 
 const schemas = {
   config: {
-    schema: 'https://raw.githubusercontent.com/runnerty/schemas/master/src/config-2.6.json'
+    schema: 'https://raw.githubusercontent.com/runnerty/schemas/master/schemas/2.8/config.json'
   },
   plan_chains_link: {
-    schema: 'https://raw.githubusercontent.com/runnerty/schemas/master/src/plan_chains_link-2.6.json'
+    schema: 'https://raw.githubusercontent.com/runnerty/schemas/master/schemas/2.8/plan-chains-link.json'
   },
   chains: {
-    schema: 'https://raw.githubusercontent.com/runnerty/schemas/master/src/chain-2.6.json'
+    schema: 'https://raw.githubusercontent.com/runnerty/schemas/master/schemas/2.8/chain.json'
   }
 };
 
